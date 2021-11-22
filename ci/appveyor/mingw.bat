23aeec4d751dbdd0967c01e79c7b29c492c5eb4a
sdarwin <samuel.d.darwin@gmail.com>
2021-11-22 15:39:43 -0600

    test

--- ci/appveyor/mingw.bat
+++ ci/appveyor/mingw.bat
@@ -33,18 +33,19 @@ echo fi
 echo if [ "$upgradepacman" = "yes" ] ; then
 echo     echo "Now upgrading pacman"
 echo     echo "Keys:"
-REM echo     curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz
-REM echo     curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig
-REM echo     pacman-key --verify msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig
-REM echo     pacman --noconfirm -U msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz
-echo     curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-1~20201002-1-any.pkg.tar.xz
-echo     curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-1~20201002-1-any.pkg.tar.xz.sig
-echo     pacman-key --verify msys2-keyring-1~20201002-1-any.pkg.tar.xz.sig
-echo     pacman --noconfirm -U msys2-keyring-1~20201002-1-any.pkg.tar.xz
+echo     curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz
+echo     curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig
+echo     pacman-key --verify msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig
+echo     pacman --noconfirm -U msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz
+REM The following experiment could not be verified remotely
+REM echo     curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-1~20201002-1-any.pkg.tar.xz
+REM echo     curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-1~20201002-1-any.pkg.tar.xz.sig
+REM echo     pacman-key --verify msys2-keyring-1~20201002-1-any.pkg.tar.xz.sig
+REM echo     pacman --noconfirm -U msys2-keyring-1~20201002-1-any.pkg.tar.xz
 echo     echo "Packages:"
 echo     pacman --noconfirm -U "http://repo.msys2.org/msys/x86_64/libzstd-1.4.5-2-x86_64.pkg.tar.xz"
 echo     pacman --noconfirm -U "http://repo.msys2.org/msys/x86_64/zstd-1.4.5-2-x86_64.pkg.tar.xz"
-echo     pacman --noconfirm -U "http://repo.msys2.org/msys/x86_64/pacman-5.2.2-4-x86_64.pkg.tar.xz"
+echo     pacman --noconfirm -U "http://repo.msys2.org/msys/x86_64/pacman-5.2.1-12-x86_64.pkg.tar.xz"
 echo else
 echo     echo "Not upgrading pacman"
 echo fi
